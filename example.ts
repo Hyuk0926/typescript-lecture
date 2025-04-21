3164f2acfd02aa9f9bf70e221170a06008652bdc
hyuk <chldmsgur09@gmail.com>
2025-04-21 10:18:22 +0900

    TODO 주석 삭제

--- example.ts
+++ example.ts
@@ -22,13 +22,6 @@ for (const key in someSchedule) {
 
 console.log(someSchedule);
 
-//TODO: calcTotal()이라는 함수를 통해 장바구니에 있는 상품들의 총 가격을 계산하는 함수를 만들기
-//TODO: 장바구니의 각 상품은 이름, 종류, 가격, 개수 4가지의 값이 들어갈 수 있어야 함
-//TODO: 장바구니에 담긴 각 상품 타임을 정의하고, 장바구니를 인자로 받아서 총 상품 가격을 반환하는 calcTotal() 함수를 만들어주세요
-// TODO: 장바구니의 각 상품 타입정의하고, cart 배열에도 타입 부여하여 상품을 3개이상 추가
-//TODO: Category 라는 enum을 정의하고, 각 상품의 종류를 enum으로 정의하여 사용하세요
-// TODO: calcTotal 함수를 만들고 실행하여 지불해야할 총 금액을 계산하여 total에 저장하여 확인
-
 enum Category { //Category를 작성한 이유:? //cart의 type을 string으로 정의했는데, string은 너무 광범위하기에, Category라는 enum을 만들어서 그 안에 있는 값만 사용하도록 하기 위함
   FRUIT = 'fruit',
   VEGETABLE = 'vegetable',
